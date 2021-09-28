f7d0a7a756247142e4737d9f3dff2f3779b896bf
Bary Pollack <bary.pollack@baker.edu>
2021-09-28 00:09:39 -0700

    End slide 145; Scene Loading

--- Assets/Scripts/Main_Menu/MainMenu.cs
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -12,4 +12,16 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(1);      // Game Scene
         //print("LoadGame 22");
     }
+
+    public void LoadSinglePlayerGame()
+    {
+        print("Single Player Game Loading...");
+        SceneManager.LoadScene(1);      // Single Player Game Scene
+    }
+
+    public void LoadCoOpMode()
+    {
+        print("Co-Op Mode Loading...");
+        SceneManager.LoadScene(2);      // Co-Op Mode Game Scene
+    }
 }
